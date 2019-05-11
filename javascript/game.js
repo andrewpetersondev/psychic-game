8491535d5a8b2220f090e8cf0627c7bd09af6a9d
Andrew P <andrewpetersondev@gmail.com>
2019-05-10 20:53:08 -0500

    added global variables

--- javascript/game.js
+++ javascript/game.js
@@ -4,6 +4,11 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // variables for wins, losses, guesses remaining, guesses, randomLetter
+var wins = 0;
+var losses = 0; 
+var guessesRemaining = 10;
+var wrongGuesses = [];
+var randomLetter = Math.floor(Math.random * computerChoices.length);
 
 
 // ================= PLAY GAME =============================
