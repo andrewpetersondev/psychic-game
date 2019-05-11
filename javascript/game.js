5f905026f84a57284e3d41aa2d542ff0b5bd53ee
Andrew P <andrewpetersondev@gmail.com>
2019-05-10 20:43:29 -0500

    psuedocode and layout for psychic game

---
+++ javascript/game.js
@@ -0,0 +1,18 @@
+// ================ INITIALIZE ==========================
+
+// computer will select one of these choices
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// variables for wins, losses, guesses remaining, guesses, randomLetter
+
+
+// ================= PLAY GAME =============================
+
+// .onkeyup function for user to guess what the letter is 
+
+// if userGuess === computer generated random letter and guesses != 0 then increment wins + 1 and restart game
+
+// if userGuess !== computer generated random letter and guesses remaining === 0 then increment losses + 1 and restart game
+
+// code that inputs game results to html
+
