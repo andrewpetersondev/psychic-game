427f5b7cd9bb2b02e4b80296e30a747b4a1d9d26
Andrew P <andrewpetersondev@gmail.com>
2019-05-10 21:11:59 -0500

    coded what to do when the player wins, and when they lose

--- javascript/game.js
+++ javascript/game.js
@@ -5,19 +5,42 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 // variables for wins, losses, guesses remaining, guesses, randomLetter
 var wins = 0;
-var losses = 0; 
+var losses = 0;
 var guessesRemaining = 10;
 var wrongGuesses = [];
 var randomLetter = computerChoices[Math.floor(Math.random * computerChoices.length)];
+
+// create a function to restart game
+function restart {
+    guessesRemaining = 10;
+    wrongGuesses = [];
+    randomLetter = computerChoices[Math.floor(Math.random * computerChoices.length)];
+}
 
 
 // ================= PLAY GAME =============================
 
 // .onkeyup function for user to guess what the letter is 
+document.onkeyup = function playGame(event) {
+    userInput = event.key.toLowerCase();
 
-// if userGuess === computer generated random letter and guesses != 0 then increment wins + 1 and restart game
+    // if userGuess === computer generated random letter and guesses != 0 then increment wins + 1 and restart game
+    if ((userInput === randomLetter) && (guessesRemaining > 0)) {
+        wins++;
+        restart();
+        
+    } else if (guessesRemaining > 0 ) {
+        guessesRemaining--;
 
-// if userGuess !== computer generated random letter and guesses remaining === 0 then increment losses + 1 and restart game
 
-// code that inputs game results to html
 
+    } else {
+        losses++;
+        restart();
+    }
+
+    // if userGuess !== computer generated random letter and guesses remaining === 0 then increment losses + 1 and restart game
+
+    // code that inputs game results to html
+
+}
